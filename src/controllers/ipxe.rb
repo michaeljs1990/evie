4c86613d599647467cbe9add93a8a9bac36bf92e
Michael Schuett <michaeljs1990@gmail.com>
2017-01-03 03:04:34 -0500

    Intake or boot into util
    Need to do a lot more work but more work needs to be done on collins and the genesis provisioning side of things before i can continue down this path.

--- src/controllers/ipxe.rb
+++ src/controllers/ipxe.rb
@@ -4,7 +4,7 @@ class Evie::Controller::Ipxe < Evie::Controller::Base
   # genesis mode based on information we find
   # inside of collins.
   get '/:asset_tag' do
-    genesis_mode = get_genesis_mode
+    genesis_mode = get_genesis_mode params['asset_tag']
 
     erb :ipxe, :locals => {
       :genesis_mode => genesis_mode,
@@ -15,10 +15,20 @@ class Evie::Controller::Ipxe < Evie::Controller::Base
 
   # Figure out what mode genesis should be
   # booting up into.
-  def get_genesis_mode
+  def get_genesis_mode asset_tag
     collins = get_collins_client
-
-    return 'intake'
+    # Get the first element of the array
+    asset = collins.find({'TAG' => asset_tag}).first
+     
+    #  We don't know about this machine
+    # so we will try to intake it.
+    if asset.nil?
+      return 'intake'
+    end
+    
+    # If we don't know what to do boot up
+    # genesis in util mode for poking around
+    return 'util'
   end
 
 end
