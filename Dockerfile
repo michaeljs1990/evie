3ec0137dedefad6a683e14a19157ba781106008f
Michael Schuett <michaeljs1990@gmail.com>
2017-01-02 21:53:44 -0500

    Update dockerfile
    Add additional params to the dockerfile that are required when launching them.
    Start breaking out ipxe controller to allow for additional logic.

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ FROM ruby:2.3.3-alpine
 
 WORKDIR /evie
 
+# Default evie config file
+ENV EVIE_CONFIG=/evie/config/sites/general.yml
+
 # Install libs needed for sinatra extentions
 RUN apk add -U g++ libc-dev make
 
--- src/controllers/ipxe.rb
+++ src/controllers/ipxe.rb
@@ -3,14 +3,22 @@ class Evie::Controller::Ipxe < Evie::Controller::Base
   # load the ipxe menu config picking the
   # genesis mode based on information we find
   # inside of collins.
-  get '/menu' do
-    collins = get_collins_client
+  get '/:asset_tag' do
+    genesis_mode = get_genesis_mode
 
     erb :ipxe, :locals => {
-      :genesis_mode => 'intake',
+      :genesis_mode => genesis_mode,
       :initrd_img => evie['genesis']['initrd_img_url'],
       :vmlinuz => evie['genesis']['vmlinuz_url'],
       :conf => evie['genesis']['conf_url']}
+  end
+
+  # Figure out what mode genesis should be
+  # booting up into.
+  def get_genesis_mode
+    collins = get_collins_client
+
+    return 'intake'
   end
 
 end
