9ccd6b090c314a734f5c1644247eb4d13bc5321c
Michael Schuett <michaeljs1990@gmail.com>
2017-01-29 03:18:43 -0500

    Fix bugs around provisioning
    Handle errors around power on. Right now this is kind of brute force
    but it works. Should likely check and parse status and decide what to
    do in the future. Cleanup typos around accessing the config files.

--- src/helpers/provision.rb
+++ src/helpers/provision.rb
@@ -10,25 +10,35 @@ module Evie::Helper::Provision
     msg = "Evie: Provisioning #{asset.tag} as profile #{profile.profile}"
     collins.set_status!(asset, :provisioning, msg, :running)
     # Set the hostname for this computer
-    hostname = generate_hostname(profile, suffix)
+    hostname = generate_hostname(asset.tag, profile, suffix)
     collins.set_attribute!(asset, :hostname, hostname)
     password = generate_password
     collins.set_attribute!(asset, :password, password)
-    # Reboot the asset so it will start provisioning
-    collins.power!(asset, :reboothard)
+    # Start asset for provisioning
+    power_on_asset(collins, asset)
   end
 
   # Return a hostname for a given asset
   # currently we don't care about the suffix
-  def generate_hostname profile, suffix
-    site = evie['provision']['site']
-    domain = evie['provision']['domain']
-    hash = Digest::SHA1.hexdigest(asset.tag)[0...10]
+  def generate_hostname tag, profile, suffix
+    site = evie['provisioning']['site']
+    domain = evie['provisioning']['domain']
+    hash = Digest::SHA1.hexdigest(tag)[0...10]
     return "#{profile.prefix}-#{hash}.#{site}.#{domain}".downcase
   end
 
   def generate_password
     return SecureRandom.urlsafe_base64(12)
+  end
+
+  def power_on_asset collins, asset
+    begin
+      # First try to reboot incase the
+      # asset is already running.
+      collins.power!(asset, :reboothard)
+    rescue Collins::RequestError
+      collins.power!(asset, :poweron)
+    end
   end
 
 end
